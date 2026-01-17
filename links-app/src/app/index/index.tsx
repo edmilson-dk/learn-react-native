41d5446c4b54c9994e595273dc3a03b91537066f
edmilson-dk <droidvidaboa@gmail.com>
2026-01-17 14:24:06 -0300

    first commit

---
+++ links-app/src/app/index/index.tsx
@@ -0,0 +1,23 @@
+import { Category } from "@/components/category";
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Image, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name='add' size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+
+      <Category name='Projetos' icon='code' isSelected />
+      <Category name='Site' icon='language' isSelected={false} />
+      <Category name='Video' icon='movie' isSelected={false} />
+    </View>
+  );
+}
