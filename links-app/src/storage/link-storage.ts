0f95a871461876e43d8b3eede743666d093989bd
edmilson-dk <droidvidaboa@gmail.com>
2026-01-19 10:30:56 -0300

    feat: created get and save links in async storage

---
+++ links-app/src/storage/link-storage.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINKS_STORAGE_KEY = "@linksave:links";
+
+type LinkStorage = {
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+};
+
+async function get(): Promise<LinkStorage[]> {
+  const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY);
+  const links: LinkStorage[] = storage ? JSON.parse(storage) : [];
+  return links;
+}
+
+async function save(link: LinkStorage): Promise<void> {
+  try {
+    const links = await get();
+    links.push(link);
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify(links));
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const linkStorage = {
+  get,
+  save,
+};
